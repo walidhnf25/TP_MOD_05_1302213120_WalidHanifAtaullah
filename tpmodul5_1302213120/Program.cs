8f345bf0b849c6e2d79dc15a5aa75dbb5a2d9dfb
Walid Hanif Ataullah <walidhnf12@gmail.com>
2023-03-19 17:40:48 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302213120/Program.cs
+++ tpmodul5_1302213120/Program.cs
@@ -10,7 +10,26 @@ namespace tpmodul5_1302213120
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Panggil method PrintData dari class DataGeneric
+            DataGeneric<string> dataString = new DataGeneric<string>("1302213120");
+            dataString.PrintData();
+        }
+    }
+
+
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
         }
     }
 }
